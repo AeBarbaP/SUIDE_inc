8c7f534827e5125f2be954a6aab776f622bf3c37
AnnyBarba <aebarbap@gmail.com>
2022-08-31 15:11:35 -0500

    consulta tabla documentos en index lista, faltan los modales

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <title>Dashboard Template · Bootstrap v5.2</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/dashboard/">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
@@ -88,7 +88,7 @@
   <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
   <div class="navbar-nav">
     <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="#">Sign out</a>
+      <a class="nav-link px-3" href="#">Cerrar Sesión</a>
     </div>
   </div>
 </header>
@@ -172,9 +172,8 @@
     </nav>
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h3 class="text-muted">Sistema Único de Identificación y Verificación</h3>
-        <h1>Bienvenido</h1>
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 ">
+        <p class="h3 text-muted">Sistema Único de Identificación y Verificación</p>
         <div class="btn-toolbar mb-2 mb-md-0">
           <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
             <span data-feather="calendar" class="align-text-bottom"></span>
@@ -182,7 +181,133 @@
           </button>
         </div>
       </div>
+      <p class="h1 mb-3">Bienvenido</p>
+      <hr>
+      <h4 class="text-muted mt-3">Últimas credenciales generadas</h4>
       <div class="table-responsive">
+            <table class="table table-hover table-bordered table-sm align-middle mt-4">
+              <thead style="background-color:#7B8DAB;" class="text-light align-middle">
+                <tr class="text-center">
+                    <th scope="col">#</th>
+                    <th scope="col">No. de Expediente</th>
+                    <th scope="col">Fecha de Entrega</th>
+                    <th scope="col">Vigencia</th>
+                    <th scope="col">Usuario</th>
+                    <th scope="col">Credencial</th>
+                    <th scope="col">Tarjetón</th>
+                </tr>
+              </thead>
+              <tbody id="myTable">
+                <?php
+                  include('prcd/query.php');
+                  $x = 0;
+                  while ($row_sqlQueryCredencial = $resultadoQueryCredencial->fetch_assoc()) {
+                    $x++;
+                    echo '
+                    <input id="imprime2" value="'.$row_sqlQueryCredencial['id'].'" hidden>
+                    <tr class="text-center bg-white">
+                      <td>' . $x . '</td>
+                      <td>' . $row_sqlQueryCredencial['id_ext'] . '</td>
+                      <td>' . $row_sqlQueryCredencial['fecha_c'] . '</td>
+                      <td>' . $row_sqlQueryCredencial['vigencia_cred'] . '</td>';
+                      $idusers= $row_sqlQueryCredencial['id_users'];
+                      $sqlUser= "SELECT * FROM users WHERE id ='$idusers'";
+                      $resultadoQueryUser = $conn->query($sqlUser);
+                      $row_sqlQueryUser = $resultadoQueryUser->fetch_assoc();
+                      echo '
+                      <td>' . $row_sqlQueryUser['nombre'] . '</td>';
+
+                      if($row_sqlQueryCredencial['entregado_c'] == 1){
+                        echo '
+                        <td><a href="data-bs-toggle="modal" data-bs-target="#QR'.$row_sqlQueryCredencial['id'].'""><i class="h4 bi bi-check text-success"></i></a></td>';
+                      } elseif($row_sqlQueryCredencial['entregado_c'] == 0){
+                        echo '
+                        <td><a href="data-bs-toggle="modal" data-bs-target="#QR'.$row_sqlQueryCredencial['id'].'""><i class="h4 bi bi-x text-danger"></a></i></td>';
+                      }
+
+                      if($row_sqlQueryCredencial['entregado_t'] == 1){
+                        echo '
+                        <td><a href="data-bs-toggle="modal" data-bs-target="#QR'.$row_sqlQueryCredencial['id'].'""><i class="h4 bi bi-check text-success"></i></a></td>';
+                      } elseif($row_sqlQueryCredencial['entregado_t'] == 0){
+                        echo '
+                        <td><a href="data-bs-toggle="modal" data-bs-target="#QR'.$row_sqlQueryCredencial['id'].'""><i class="h4 bi bi-x text-danger"></i></a></td>';
+                      }
+                      echo '
+                    <tr>
+                    <!-- Modal -->
+                    <div class="modal fade" id="QR'.$row_sqlQueryCredencial['id'].'" tabindex="-1" aria-labelledby="QRLabel" aria-hidden="true">
+                      <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel"><i class="bi bi-qr-code"></i> Información QR</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body" style="text-align: center;" id="div_print'.$row_sqlQueryCredencial['id'].'">
+                            <center><img src="img/logomorismas.png" height="150"></center>
+                            <br>
+                            <center><h5 style="font-size: 1.5rem"><strong>Número de Expediente:</strong> ' . $row_sqlQueryCredencial['id_ext'] . ' </h5>
+                            <h5 style="font-size: 1.5rem"><strong>Fecha de expedición:</strong> ' . $row_sqlQueryCredencial['fecha_c'] . '</h5>
+                            <h5 style="font-size: 1.5rem"><strong>Expira:</strong> ' . $row_sqlQueryCredencial['vigencia_cred'] . ' </h5>
+                            <h5 style="font-size: 1.5rem"><strong>Atenidod por:</strong> ' . $row_sqlQueryCredencial['id_users'] . '</h5>
+                            <h5 style="font-size: 1.5rem"><strong></strong></h5></center>
+                            <p class="text-center"><img src="prcd/QR/codes/'. $row_sqlQueryCredencial['qr_cred'].'"></p>
+                          </div>
+                          <div class="modal-footer">';?>
+                          <!-- <a type="button" class="btn btn-primary" href="javascript:imprimirSeleccion('div_print<?php echo $row_sqlQuery['id']?>')"><i class="bi bi-printer-fill"></i> Imprimir</a> -->
+                          <?php echo '
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <!-- Modal editar-->
+                    <div class="modal fade" id="editar'.$row_sqlQuery['id'].'" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      <div class="modal-dialog">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel"><i class="bi bi-person-plus"></i> Alta de polvora</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                            <form action="prcd/actualizar.php" method="POST"><!--form-->
+                                  <input name="id" value="'.$row_sqlQuery['id'].'" hidden>
+                                  <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-person"></i></span>
+                                    <input type="text" class="form-control" placeholder="Nombre" aria-label="Nombre" value="' . $row_sqlQuery['nombre'] . '" aria-describedby="basic-addon1" name="nombre" required>
+                                  </div>
+                                  <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-workspace"></i></span>
+                                    <input type="text" class="form-control" placeholder="Apellidos" aria-label="Apellidos" value="' . $row_sqlQuery['apellidos'] . '" aria-describedby="basic-addon1"  name="apellidos" required>
+                                  </div>
+                                  <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-badge"></i></span>
+                                    <input type="text" class="form-control" placeholder="CURP" aria-label="CURP" aria-describedby="basic-addon1" value="' . $row_sqlQuery['curp'] . '" name="curp" readonly>
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-123"></i></span>
+                                    <input type="text" class="form-control" placeholder="Cantidad" aria-label="Cantidad" value="' . $row_sqlQuery['cantidad_polvora'] . '" aria-describedby="basic-addon1" maxlength="1" onkeypress="ValidaSoloNumeros()" name="cantidad_polvora" readonly>
+                                  </div><!-- Si, y solo si se asignan 2kg de polvora, se habilita el campo de detalles y se convierte en obligatorio -->
+                                  
+                                  <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-card-text"></i></span>
+                                    <textarea style="resize: none;" rows="4" type="text" class="form-control" placeholder="Detalles (opcional)" aria-label="Detalles" aria-describedby="basic-addon1" name="detalles">' . $row_sqlQuery['detalles'] . '</textarea>
+                                  </div>
+                            </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><i class="bi bi-x-circle-fill"></i> Cancelar</button>
+                                <button type="submit" class="btn btn-primary"><i class="bi bi-person-plus"></i> Guardar</button>
+                              </div>
+                            </form><!--form-->
+                        </div>
+                      </div>
+                    </div>
+                    ';
+                    
+                  }
+            echo'</table>';
+            ?>
+          </div>
+
+      <!-- <div class="table-responsive">
         <table class="table table-striped table-sm">
           <thead>
             <tr>
@@ -308,7 +433,7 @@
             </tr>
           </tbody>
         </table>
-      </div>
+      </div> -->
     </main>
   </div>
 </div>
--- prcd/query.php
+++ prcd/query.php
@@ -7,17 +7,17 @@
 <?php
     include('prcd/qc/qc.php');
 
-    $sqlQueryCredencial ="SELECT * FROM documentos WHERE entregado_c = 0 ORDER BY fecha_c ASC";
+    $sqlQueryCredencial ="SELECT * FROM documentos WHERE entregado_c = 1 ORDER BY fecha_c ASC";
     $resultadoQueryCredencial = $conn->query($sqlQueryCredencial);
 
-    $sqlQueryTarjeton ="SELECT * FROM documentos WHERE entregado_t = 0 ORDER BY fecha_t ASC";
+    $sqlQueryTarjeton ="SELECT * FROM documentos WHERE entregado_t = 1 ORDER BY fecha_t ASC";
     $resultadoQueryTarjeton = $conn->query($sqlQueryTarjeton);
 
-    $sqlQueryEntregadoC ="SELECT * FROM documentos WHERE entregado_c = 1 ORDER BY fecha_c ASC";
+/*     $sqlQueryEntregadoC ="SELECT * FROM documentos WHERE entregado_c = 1 ORDER BY fecha_c ASC";
     $resultadoQueryEntregadoC = $conn->query($sqlQueryEntregadoC);
 
     $sqlQueryEntregadoT ="SELECT * FROM documentos WHERE entregado_t = 1 ORDER BY fecha_t ASC";
-    $resultadoQueryEntregadoT = $conn->query($sqlQueryEntregadoT);
+    $resultadoQueryEntregadoT = $conn->query($sqlQueryEntregadoT); */
 ?>
 
 </html>
