174087e67f647d7bf072f4d347f5f9dd1c86e5c2
jesus rlv <jesusrlv@iMac-de-jesus.local>
2023-01-04 14:50:36 -0600

    cambio de ruta de script y adecuacion automatica de busqueda de usuario

---
+++ script.js
@@ -0,0 +1,28 @@
+function abrirCamara(){
+
+    let scanner = new Instascan.Scanner({video:document.getElementById('preview') });
+    scanner.addListener('scan', function (content) {
+      console.log(content);
+    });
+    Instascan.Camera.getCameras().then(function(cameras) {
+      if (cameras.length > 0) {
+        scanner.start(cameras[0]);
+      } else {
+        // console.error('No cameras found.');
+        alert("No se encontró cámara");
+      }
+    }).catch(function (e){
+      console.error(e);
+    }); 
+
+    scanner.addListener('scan',function(c){
+      document.getElementById('textQR').value=c;
+      document.getElementById("myAudio").play();
+      scanner.stop();
+      });
+
+      $('#validate').on('hide.bs.modal', function () { 
+        scanner.stop();
+      });
+
+  }
