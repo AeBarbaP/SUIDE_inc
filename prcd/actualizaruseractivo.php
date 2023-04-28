f49d1cc31b33dbeeff63a2649f4c855306bf1a0e
AnnyBarba <aebarbap@gmail.com>
2023-04-28 15:43:51 -0500

    error en el update de perfil activo

---
+++ prcd/actualizaruseractivo.php
@@ -0,0 +1,45 @@
+<html>
+    <header>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    </header>
+<body>
+
+<?php
+include('qc/qc.php');
+
+$id = $_POST['id'];
+$pwd = $_POST['pwd'];
+$nombre = $_POST['nombre'];
+/* $perfil = $_POST['perfilselect']; */
+$estatusUsr = $_POST['btnradio'];
+
+date_default_timezone_set('America/Mexico_City');
+setlocale(LC_TIME, 'es_MX.UTF-8');
+
+$fecha_creacion = strftime("%Y-%m-%d,%H:%M:%S");
+
+
+    /* $sqlinsert= "INSERT INTO asistentes(nombre,apellidos,curp,detalles,cantidad_polvora,entregado) VALUES('$nombre','$apellidos','$curp','$detalles','$cantidad_polvora','$entregado')"; */
+    $sqlUpdate ="UPDATE users SET pwd='$pwd', nombre='$nombre' WHERE id='$id'";
+    $resultado= $conn->query($sqlUpdate);
+
+
+    if($resultado){
+        
+        echo "<script type=\"text/javascript\">
+        Swal.fire({
+            icon: 'success',
+            title: 'Registro correcto',
+            text: 'Actualizado',
+            footer: 'SUIDEV - INCLUSIÓN 2022-2027'
+        }).then(function(){window.location='../dashboard.php';});</script>";
+        }
+        else{
+        echo 'No se registró producto';
+        }
+
+
+?>
+
+</body>
+</html>
