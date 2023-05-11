c4d7a1af2498a7ce8fcb7b46ce53972b6388dbad
AnnyBarba <aebarbap@gmail.com>
2023-05-11 15:23:26 -0500

    frnt padron done

--- prcd/suidevdb.sql
+++ prcd/suidevdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 06, 2023 at 10:30 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: May 11, 2023 at 10:21 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,85 +24,77 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `alergia_descripcion`
---
-
-CREATE TABLE `alergia_descripcion` (
-  `id` int(11) NOT NULL,
-  `id_documentos` int(11) NOT NULL COMMENT 'Se relaciona con el id de la tabla documentos',
-  `descripcion` varchar(150) NOT NULL,
-  `id_alergia_tipo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `alergia_tipo`
---
-
-CREATE TABLE `alergia_tipo` (
-  `id` int(11) NOT NULL,
-  `tipo` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `alergia_tipo`
---
-
-INSERT INTO `alergia_tipo` (`id`, `tipo`) VALUES
-(1, 'Ambiental'),
-(2, 'Alimentaria'),
-(3, 'Intradérmica'),
-(4, 'Por contacto'),
-(5, 'Farmacológica');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `datos_generales`
 --
 
 CREATE TABLE `datos_generales` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `ap_paterno` varchar(50) NOT NULL,
-  `ap_materno` varchar(50) DEFAULT NULL,
-  `tipo_discapacidad` int(11) NOT NULL,
-  `curp` varchar(16) NOT NULL,
+  `apellido_p` varchar(50) NOT NULL,
+  `apellido_m` varchar(50) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `edo_civil` int(11) NOT NULL,
+  `f_nacimiento` date NOT NULL,
+  `lugar_nacimiento` int(200) NOT NULL COMMENT 'Estados',
   `domicilio` varchar(100) NOT NULL,
-  `colonia` varchar(50) NOT NULL,
+  `no_int` varchar(10) NOT NULL,
+  `no_ext` varchar(10) NOT NULL,
+  `colonia` varchar(100) NOT NULL,
+  `entre_vialidades` varchar(100) NOT NULL,
+  `descr_referencias` varchar(200) NOT NULL,
   `localidad` int(11) NOT NULL,
   `municipio` int(11) NOT NULL,
-  `estado` int(11) NOT NULL,
   `cp` int(11) NOT NULL,
-  `tipo_sangre` int(2) NOT NULL,
-  `tipo_alergia` int(2) NOT NULL,
-  `id_ext` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `telefono_part` varchar(20) NOT NULL,
+  `telefono_cel` varchar(20) NOT NULL,
+  `escolaridad` int(11) NOT NULL,
+  `profesión` varchar(50) NOT NULL,
+  `curp` varchar(30) NOT NULL,
+  `rfc` varchar(30) NOT NULL,
+  `estudia` int(11) NOT NULL,
+  `estudia_donde` varchar(100) NOT NULL,
+  `estudia_habilidad` varchar(100) NOT NULL,
+  `trabaja` varchar(100) NOT NULL,
+  `trabaja_donde` int(11) NOT NULL,
+  `trabaja_ingresos` varchar(10) NOT NULL,
+  `asoc_civ` int(11) NOT NULL,
+  `asoc_cual` varchar(100) NOT NULL,
+  `pensionado` int(11) NOT NULL,
+  `pensionado_donde` varchar(100) NOT NULL,
+  `seguridad_social` int(11) NOT NULL,
+  `seguridad_social_donde` varchar(100) NOT NULL,
+  `seguridad_social_otro` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `discapacidad_descripcion`
+-- Table structure for table `datos_medicos`
 --
 
-CREATE TABLE `discapacidad_descripcion` (
+CREATE TABLE `datos_medicos` (
   `id` int(11) NOT NULL,
-  `id_discapacidadtipo` int(11) NOT NULL,
-  `descripcion` varchar(100) NOT NULL,
-  `nota` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `discapacidad_tipo`
---
-
-CREATE TABLE `discapacidad_tipo` (
-  `id` int(11) NOT NULL,
-  `tipo` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `discapacidad` varchar(50) NOT NULL,
+  `grado_discapacidad` int(11) NOT NULL,
+  `tipo_discapacidad` int(11) NOT NULL,
+  `causa` int(11) NOT NULL,
+  `causa_otro` varchar(100) NOT NULL,
+  `temporalidad` int(11) NOT NULL,
+  `valoracion` int(11) NOT NULL,
+  `fuente_valoracion` int(11) NOT NULL,
+  `rehabilitacion` int(11) NOT NULL,
+  `rehabilitacion_donde` varchar(50) NOT NULL,
+  `rehabilitacion_inicio` date NOT NULL,
+  `rehabilitacion_fin` date NOT NULL,
+  `tipo_sangre` int(11) NOT NULL,
+  `cirugias` int(11) NOT NULL,
+  `tipo_cirugias` varchar(100) NOT NULL,
+  `enfermedades` int(11) NOT NULL,
+  `medicamentos` int(11) NOT NULL,
+  `alergias` int(11) NOT NULL,
+  `protesis` int(11) NOT NULL,
+  `protesis_tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -112,49 +104,35 @@ CREATE TABLE `discapacidad_tipo` (
 
 CREATE TABLE `documentos` (
   `id` int(11) NOT NULL,
-  `id_ext` varchar(50) NOT NULL,
-  `concatenado_id` varchar(50) NOT NULL,
-  `qr_cred` varchar(100) NOT NULL,
-  `qr_tarjeton` varchar(100) NOT NULL,
-  `entregado_c` int(10) NOT NULL,
-  `fecha_c` date DEFAULT NULL,
-  `entregado_t` int(10) DEFAULT NULL,
-  `fecha_t` date DEFAULT NULL,
-  `vigencia_cred` date DEFAULT NULL,
-  `vigencia_tarjeton` date DEFAULT NULL,
-  `id_users` int(15) NOT NULL,
-  `recibe` varchar(50) NOT NULL,
-  `folio_cred` int(10) DEFAULT NULL,
-  `folio_tarj` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `documentos`
---
-
-INSERT INTO `documentos` (`id`, `id_ext`, `concatenado_id`, `qr_cred`, `qr_tarjeton`, `entregado_c`, `fecha_c`, `entregado_t`, `fecha_t`, `vigencia_cred`, `vigencia_tarjeton`, `id_users`, `recibe`, `folio_cred`, `folio_tarj`) VALUES
-(1, 'C-4940-44790', '', '', '', 1, '2022-07-06', 0, '0000-00-00', '2023-07-06', '0000-00-00', 1, '', 0, 0),
-(2, 'C-4940-44790', 'C-4940-44790-1', '-', '-', 1, '2022-08-31', 1, '2022-08-31', '2023-08-31', '2023-08-31', 1, '', 0, 0);
+  `id_ext` int(11) NOT NULL,
+  `doc_medico` varchar(50) NOT NULL,
+  `doc_acta_nac` varchar(50) NOT NULL,
+  `doc_curp` varchar(50) NOT NULL,
+  `doc_ine` varchar(50) NOT NULL,
+  `doc_comp_dom` varchar(50) NOT NULL,
+  `doc_tarjeta` varchar(50) NOT NULL,
+  `doc_foto1` varchar(50) NOT NULL,
+  `doc_foto2` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `estudio_se`
+-- Table structure for table `integracion`
 --
 
-CREATE TABLE `estudio_se` (
+CREATE TABLE `integracion` (
   `id` int(11) NOT NULL,
-  `estudios` varchar(50) DEFAULT NULL,
-  `trabajo` varchar(100) DEFAULT NULL,
-  `asociacion` varchar(100) DEFAULT NULL,
-  `sindicato` varchar(100) DEFAULT NULL,
-  `id_istitucionpension` int(11) DEFAULT NULL,
-  `monto_pension` varchar(10) DEFAULT NULL COMMENT 'Agregar bloqueo de pensión',
-  `id_institucioncredito` int(11) DEFAULT NULL,
-  `monto_credito` varchar(10) DEFAULT NULL COMMENT 'Agregar bloqueo',
-  `id_documentos` int(11) NOT NULL,
-  `id_ingresomensual` varchar(10) DEFAULT NULL COMMENT 'agregar bloqueo'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_ext` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `parentesco` int(11) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `escolaridad` int(11) NOT NULL,
+  `profesion_oficio` int(11) NOT NULL,
+  `discapacidad` int(11) NOT NULL,
+  `discapacidad_tipo` varchar(50) NOT NULL,
+  `ingreso` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -169,7 +147,7 @@ CREATE TABLE `log_entregas` (
   `id_users` int(10) NOT NULL,
   `id_documentos` varchar(10) NOT NULL,
   `tipo_entrega` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -182,7 +160,7 @@ CREATE TABLE `log_usrlogin` (
   `id_usr` int(11) NOT NULL,
   `fecha_iniciosesion` datetime DEFAULT NULL,
   `fecha_cierresesion` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `log_usrlogin`
@@ -207,38 +185,13 @@ INSERT INTO `log_usrlogin` (`id`, `id_usr`, `fecha_iniciosesion`, `fecha_cierres
 (16, 4, '2022-09-27 10:29:57', NULL),
 (17, 4, NULL, '2022-09-27 10:30:01'),
 (18, 1, '2022-09-27 12:11:54', NULL),
-(19, 1, '2022-09-28 09:52:02', NULL),
-(20, 1, NULL, '2022-09-28 12:30:20'),
-(21, 1, '2022-09-28 12:31:01', NULL),
-(22, 1, '2022-10-03 14:04:15', NULL),
-(23, 1, '2022-10-04 11:41:36', NULL),
-(24, 1, '2022-11-29 12:25:57', NULL),
-(25, 1, '2022-12-05 09:45:42', NULL),
-(26, 1, '2022-12-06 12:13:35', NULL),
-(27, 0, NULL, '2022-12-06 12:35:02'),
-(28, 1, '2022-12-07 08:33:41', NULL),
-(29, 1, NULL, '2022-12-07 12:55:40'),
-(30, 1, '2022-12-07 12:56:39', NULL),
-(31, 1, '2022-12-08 10:30:21', NULL),
-(32, 0, NULL, '2022-12-08 11:19:31'),
-(33, 1, '2022-12-13 09:03:35', NULL),
-(34, 1, '2022-12-14 08:42:12', NULL),
-(35, 1, NULL, '2022-12-14 08:50:08'),
-(36, 1, '2022-12-14 08:51:37', NULL),
-(37, 1, NULL, '2022-12-14 08:54:04'),
-(38, 1, '2022-12-14 08:55:47', NULL),
-(39, 1, NULL, '2022-12-14 08:56:32'),
-(40, 1, '2022-12-14 10:38:35', NULL),
-(41, 1, NULL, '2022-12-14 15:28:32'),
-(42, 1, '2022-12-15 08:22:53', NULL),
-(43, 1, NULL, '2022-12-15 14:53:10'),
-(44, 1, '2022-12-16 10:36:39', NULL),
-(45, 1, '2022-12-19 13:02:34', NULL),
-(46, 1, '2022-12-19 13:03:23', NULL),
-(47, 1, '2023-01-05 11:40:25', NULL),
-(48, 1, NULL, '2023-01-05 13:15:26'),
-(49, 1, '2023-01-05 13:27:10', NULL),
-(50, 1, '2023-01-06 14:11:02', NULL);
+(19, 1, '2022-09-27 17:03:40', NULL),
+(20, 1, '2022-11-29 15:43:37', NULL),
+(21, 1, NULL, '2022-11-29 16:30:08'),
+(22, 1, '2022-12-08 14:22:19', NULL),
+(23, 1, '2022-12-09 16:05:41', NULL),
+(24, 2, '2022-12-13 15:53:56', NULL),
+(25, 2, NULL, '2022-12-13 16:22:08');
 
 -- --------------------------------------------------------
 
@@ -249,7 +202,7 @@ INSERT INTO `log_usrlogin` (`id`, `id_usr`, `fecha_iniciosesion`, `fecha_cierres
 CREATE TABLE `perfiles_usr` (
   `id` int(11) NOT NULL,
   `perfil` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `perfiles_usr`
@@ -284,7 +237,39 @@ CREATE TABLE `prestamo` (
   `id_users` int(11) NOT NULL,
   `qr_prestamo` varchar(100) NOT NULL,
   `entregado` int(1) NOT NULL COMMENT '1 Beneficiario\r\n2 Familiar\r\n3 Enlace Municipal'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `referencias`
+--
+
+CREATE TABLE `referencias` (
+  `id` int(11) NOT NULL,
+  `id_ext` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `parentesco` int(11) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `profesion_oficio` int(11) NOT NULL,
+  `celular` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id` int(11) NOT NULL,
+  `id_ext` int(11) NOT NULL,
+  `solicitud` int(11) NOT NULL,
+  `fecha_solicitud` date NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `estatus_s` int(11) NOT NULL,
+  `fecha_entrega` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -300,7 +285,7 @@ CREATE TABLE `users` (
   `nombre` varchar(80) NOT NULL,
   `fecha_creacion` datetime NOT NULL,
   `estatus` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
@@ -310,9 +295,9 @@ INSERT INTO `users` (`id`, `username`, `pwd`, `perfil`, `nombre`, `fecha_creacio
 (1, 'annaeliza', '12345', 1, 'Ana Elisa Barba Pinedo', '0000-00-00 00:00:00', 1),
 (2, 'jrodolfo', '12345678', 1, 'Jesús Rodolfo Leaños Villegas', '2022-09-21 14:23:40', 1),
 (4, 'juanjo', '12345678', 2, 'Juan José Quiroz Nava', '2022-09-21 14:34:03', 1),
-(5, 'grisgalvan', '789456123', 2, 'Griselda Galván Galván', '2022-09-21 15:08:58', 2),
+(5, 'grisgalvan', '789456123', 1, 'Griselda Galván Galván', '2022-09-21 15:08:58', 1),
 (6, 'amparoi', '654213', 2, 'Amparo Iturriaga Araiza', '2022-09-22 12:56:45', 1),
-(7, 'hecmendoza', '123456789', 2, 'Héctor Mario Mendoza Bañuelos', '2022-09-22 14:04:40', 1);
+(7, 'hecmendoza', '123456789', 2, 'Héctor Mario Mendoza Bañuelos', '2022-09-22 14:04:40', 2);
 
 -- --------------------------------------------------------
 
@@ -328,25 +313,55 @@ CREATE TABLE `vehiculos` (
   `marca` varchar(15) NOT NULL,
   `annio` int(4) NOT NULL,
   `no_placas` varchar(10) NOT NULL,
-  `chofer` varchar(100) NOT NULL,
-  `no_serie` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `chofer` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vivienda`
+--
+
+CREATE TABLE `vivienda` (
+  `id` int(11) NOT NULL,
+  `id_ext` int(11) NOT NULL,
+  `vivienda` int(11) NOT NULL,
+  `vivienda_renta` int(11) NOT NULL,
+  `vivienda_pagando` int(11) NOT NULL,
+  `caracteristicas` int(11) NOT NULL,
+  `caracteristicas_otro` varchar(50) NOT NULL,
+  `num_habitaciones` int(11) NOT NULL,
+  `vivienda_cocia` int(11) NOT NULL,
+  `vivienda_sala` int(11) NOT NULL,
+  `vivienda_banio` int(11) NOT NULL,
+  `vivienda_otros` varchar(100) NOT NULL,
+  `techo` int(11) NOT NULL,
+  `techo_otro` varchar(100) NOT NULL,
+  `serv_basicos_agua` int(11) NOT NULL,
+  `serv_basicos_luz` int(11) NOT NULL,
+  `serv_basicos_drenaje` int(11) NOT NULL,
+  `serv_basicos_cable` int(11) NOT NULL,
+  `serv_basicos_internet` int(11) NOT NULL,
+  `serv_basicos_celular` int(11) NOT NULL,
+  `serv_basicos_carro` int(11) NOT NULL,
+  `serv_basicos_gas` int(11) NOT NULL,
+  `serv_basicos_telefono` int(11) NOT NULL,
+  `electrodomesticos_tv` int(11) NOT NULL,
+  `electrodomesticos_lavadora` int(11) NOT NULL,
+  `electrodomesticos_estereo` int(11) NOT NULL,
+  `electrodomesticos_microondas` int(11) NOT NULL,
+  `electrodomesticos_computadora` int(11) NOT NULL,
+  `electrodomesticos_licuadora` int(11) NOT NULL,
+  `electrodomesticos_dvd` int(11) NOT NULL,
+  `electrodomesticos_estufa` int(11) NOT NULL,
+  `personas_dependen` int(11) NOT NULL,
+  `deudas` int(11) NOT NULL,
+  `deudas_cuanto` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `alergia_descripcion`
---
-ALTER TABLE `alergia_descripcion`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `alergia_tipo`
---
-ALTER TABLE `alergia_tipo`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `datos_generales`
@@ -355,15 +370,9 @@ ALTER TABLE `datos_generales`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `discapacidad_descripcion`
+-- Indexes for table `datos_medicos`
 --
-ALTER TABLE `discapacidad_descripcion`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `discapacidad_tipo`
---
-ALTER TABLE `discapacidad_tipo`
+ALTER TABLE `datos_medicos`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -373,9 +382,9 @@ ALTER TABLE `documentos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `estudio_se`
+-- Indexes for table `integracion`
 --
-ALTER TABLE `estudio_se`
+ALTER TABLE `integracion`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -403,6 +412,18 @@ ALTER TABLE `prestamo`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `referencias`
+--
+ALTER TABLE `referencias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -415,20 +436,14 @@ ALTER TABLE `vehiculos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `vivienda`
+--
+ALTER TABLE `vivienda`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `alergia_descripcion`
---
-ALTER TABLE `alergia_descripcion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `alergia_tipo`
---
-ALTER TABLE `alergia_tipo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `datos_generales`
@@ -437,27 +452,21 @@ ALTER TABLE `datos_generales`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `discapacidad_descripcion`
+-- AUTO_INCREMENT for table `datos_medicos`
 --
-ALTER TABLE `discapacidad_descripcion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `discapacidad_tipo`
---
-ALTER TABLE `discapacidad_tipo`
+ALTER TABLE `datos_medicos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `documentos`
 --
 ALTER TABLE `documentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `estudio_se`
+-- AUTO_INCREMENT for table `integracion`
 --
-ALTER TABLE `estudio_se`
+ALTER TABLE `integracion`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -470,7 +479,7 @@ ALTER TABLE `log_entregas`
 -- AUTO_INCREMENT for table `log_usrlogin`
 --
 ALTER TABLE `log_usrlogin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `perfiles_usr`
@@ -485,6 +494,18 @@ ALTER TABLE `prestamo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `referencias`
+--
+ALTER TABLE `referencias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
@@ -494,6 +515,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `vehiculos`
 --
 ALTER TABLE `vehiculos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vivienda`
+--
+ALTER TABLE `vivienda`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
