3ba7995602e11033b12ced3911998b97986b41a1
AnnyBarba <aebarbap@gmail.com>
2023-07-20 09:47:52 -0500

    actaER_PDF casi lista, falta insertar tabla

--- prcd/actaER_PDF.php
+++ prcd/actaER_PDF.php
@@ -1,317 +1,147 @@
 <?php
-//ob_start();
-//ini_set('display_startup_errors',1);
-//ini_set('display_errors',1);
-//error_reporting(-1);
-ini_set('max_execution_time', '0');
-ini_set('memory_limit', '2048M');
+require('fpdf/fpdf.php');
 
-include('prcd/qc.php');
-
-$folio = $_REQUEST['folio'];
-$internet = 0;
-$inst_periferico = 0;
-$limp_equipo = 0;
-$tec_mouse = 0;
-$falla_monitor = 0;
-$otra1 = 0;
-$act_office = 0;
-$activar_so = 0;
-$actualizar_sw = 0;
-$formateo_completo = 0;
-$limpieza_virus = 0;
-$instalar_sw = 0;
-$otra_sw = 0;
-$escanear = 0;
-$printcolor = 0;
-$rw_cd = 0;
-$web = 0;
-$otra_2 = 0;
-
-$sql = "SELECT * FROM bitacora WHERE folio = '$folio'";
-$resultadoSql = $conn->query($sql);
-$rowSQL = $resultadoSql->fetch_assoc();
-
-$sql2 = "SELECT * FROM observaciones WHERE folio = '$folio'";
-$resultadoSql2 = $conn->query($sql2);
-/* $rowSQL2 = $resultadoSql2->fetch_assoc(); */
-
-while ($rowSQL2 = $resultadoSql2->fetch_assoc()){
-  if ($rowSQL2['sub_cat']==1){
-    $internet = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $internet = "";
-  }
-  if ($rowSQL2['sub_cat']==2){
-    $inst_periferico = $rowSQL2['observaciones_dti'];
-  }   else if ($rowSQL2['sub_cat']== null){
-    $inst_periferico = "";
-  }
-  if ($rowSQL2['sub_cat']==3){
-    $limp_equipo = $rowSQL2['observaciones_dti'];
-  }   else if ($rowSQL2['sub_cat']== null){
-    $limp_equipo = "";
-  }
-  if ($rowSQL2['sub_cat']==4){
-    $tec_mouse = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $tec_mouse = "";
-  }
-  if ($rowSQL2['sub_cat']==5){
-    $falla_monitor = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $falla_monitor = "";
-  }
-  if ($rowSQL2['sub_cat']==6){
-    $otra1 = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $otra1 = "";
-  }
-  if ($rowSQL2['sub_cat']==7){
-    $act_office = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $act_office = "";
-  }
-  if ($rowSQL2['sub_cat']==8){
-    $activar_so = $rowSQL2['observaciones_dti'];
-  }   else if ($rowSQL2['sub_cat']== null){
-    $activar_so = "";
-  }
-  if ($rowSQL2['sub_cat']==9){
-    $actualizar_sw = $rowSQL2['observaciones_dti'];
-  }   else if ($rowSQL2['sub_cat']== null){
-    $actualizar_sw = "";
-  }
-  if ($rowSQL2['sub_cat']==10){
-    $formateo_completo = $rowSQL2['observaciones_dti'];
-  }   else if ($rowSQL2['sub_cat']== null){
-    $formateo_completo = "";
-  }
-  if ($rowSQL2['sub_cat']==11){
-    $limpieza_virus = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $limpieza_virus = "";
-  }
-    if ($rowSQL2['sub_cat']==12){
-    $instalar_sw = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $instalar_sw = "";
-  }
-    if ($rowSQL2['sub_cat']==13){
-    $otra_sw = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $otra_sw = "";
-  }
-    if ($rowSQL2['sub_cat']==14){
-    $escanear = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $escanear = "";
-  }
-    if ($rowSQL2['sub_cat']==15){
-    $printcolor = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $printcolor = "";
-  }
-    if ($rowSQL2['sub_cat']==16){
-    $rw_cd = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $rw_cd = "";
-  }
-    if ($rowSQL2['sub_cat']==17){
-    $web = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $web = "";
-  }
-    if ($rowSQL2['sub_cat']==18){
-    $otra_2 = $rowSQL2['observaciones_dti'];
-  }  else if ($rowSQL2['sub_cat']== null){
-    $otra_2 = "";
-  }
-/*   echo '
-  <script>
-  console.log('.$rowSQL2['observaciones_dti'].');
-  </script>
-  '; */
-}
-require('prcd/fpdf/fpdf.php');
 class PDF extends FPDF
 {
 // Cabecera de página
 function Header()
 {
     // Logo
-
-    $this->Image('img/logo_completo.jpg',72,15,63);
+    $this->Image('../img/Logos.png',10,10,100);
     // Arial bold 15
     $this->SetFont('Arial','B',15);
+    $this->Ln(5);
     // Movernos a la derecha
-    $this->Cell(80);
+    $this->Cell(120);
     // Título
+    $this->Cell(30,10,'ACTA DE ENTREGA',0,0,'L');
     // Salto de línea
-    $this->Ln(20);
+    $this->Ln(10);
 }
 
 // Pie de página
 function Footer()
 {
     // Posición: a 1,5 cm del final
-    $this->SetY(-15);
+    $this->SetY(-35);
     // Arial italic 8
     $this->SetFont('Arial','I',8);
     // Número de página
-    $this->Cell(0,10,utf8_decode('Página ').$this->PageNo().'/{nb}',0,0,'C');
+    $this->Cell(0,10,utf8_decode('Página ').$this->PageNo().'/{nb}',0,0,'R');
+    $this->Ln();
+    $this->SetFont('Arial','B',8);
+    $this->Cell(0,4,utf8_decode('Circuito Cerro del Gato S/N, Edificio K, Nivel 2'),0,0,'R');
+    $this->Ln();
+    $this->Cell(0,4,utf8_decode('Ciudad Administrativa, C.P. 98160, Zacatecas, Zac.'),0,0,'R');
+    $this->Ln();
+    $this->Cell(0,4,utf8_decode('inclusion@zacatecas.gob.mx Tels. 4924915088 y 89'),0,0,'R');
 }
 }
+
 // Creación del objeto de la clase heredada
-$pdf = new PDF();
+$pdf = new PDF('P','mm','Letter');
 $pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',10);
+$pdf->SetFont('Arial','B',11);
 $pdf->Multicell(190,8,utf8_decode('
+DIRECCIÓN DE ATENCIÓN PRIORITARIA A PERSONAS CON DISCAPACIDAD'),0,'C',0);
 
-DEPARTAMENTO DE TECNOLOGÍAS DE LA INFORMACIÓN'),0,'C',0);
-$pdf->SetFont('Arial','B',8);
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(17,7,utf8_decode('FECHA: '),0,0,'C');
+$pdf->Cell(5,7,utf8_decode(''),0,0,'C');
+$pdf->Cell(55,7,'',0,0,'C');
+$pdf->Line(10, 48, 210-10, 48); // 20mm from each edge
+$pdf->Ln();
+$pdf->Ln();
 
-$pdf->Multicell(190,5,utf8_decode('FORMATO DE SERVICIO, APOYO Y/O SOPORTE TÉCNICO'),0,'C',0);
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(44,5,utf8_decode('Número de Expediente: '),0,0,'L');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(44,5,utf8_decode(''),0,0,'C');
+$pdf->Cell(5,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(12,5,utf8_decode('CURP:'),0,0,'L');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(63,5,utf8_decode(''),0,0,'C');
 $pdf->Ln();
-$pdf->Ln();
-$pdf->SetFont('Arial','B',10);
-$pdf->Cell(151,5,'',0,0);
-$pdf->Cell(40,5,'Fecha',1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','B',8);
-$pdf->Cell(151,8,'Zacatecas, Zac., a ',0,'R',0);
-$pdf->Cell(40,8,$rowSQL['fecha'],1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','B',10);
-$pdf->SetFillColor(93,109,126);
-$pdf->Cell(100,5,'Datos del equipo',1,0,'C');
-$pdf->Cell(91,5,'Folio',1,0,'C');
-$pdf->Ln();
-$pdf->Cell(100,18,$rowSQL['datos_pc'],1,0,'C');
-$pdf->Cell(91,18,$rowSQL['folio'],1,0,'C');
-$pdf->Ln();
-$pdf->Cell(191,8,'MANTENIMIENTO PREVENTIVO/CORRECTIVO DE HARDWARE',1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','B',8);
-$pdf->Cell(100,5,'Actividad',1,0,'C');
-$pdf->Cell(91,5,'Trabajo realizado',1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(10,5,'1',1,0,'C');
-$pdf->Cell(90,5,utf8_decode('Revisar conexión a internet'),1,0,'L');
-$pdf->Cell(91,5,utf8_decode($internet),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'2',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,'Instalar impresora/scanner',1,0,'L');
-$pdf->Cell(91,5,utf8_decode($inst_periferico),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'3',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,'Limpieza interna de equipo',1,0,'L');
-$pdf->Cell(91,5,utf8_decode($limp_equipo),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'4',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,'Mouse/teclado no funciona',1,0,'L');
-$pdf->Cell(91,5,utf8_decode($tec_mouse),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'5',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,'Falla del monitor/pantalla',1,0,'L');
-$pdf->Cell(91,5,utf8_decode($falla_monitor),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'6',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,'Otra',1,0,'L');
-$pdf->Cell(91,5,utf8_decode($otra1),1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','B',10);
-$pdf->Cell(191,8,'MANTENIMIENTO PREVENTIVO/CORRECTIVO DE SOFTWARE',1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(10,5,'1',1,0,'C');
-$pdf->Cell(90,5,utf8_decode('Activación Office'),1,0,'L');
-$pdf->Cell(91,5,utf8_decode($act_office),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'2',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,utf8_decode('Activación de Sistema Operativo'),1,0,'L');
-$pdf->Cell(91,5,utf8_decode($activar_so),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'3',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,utf8_decode('Actualizar Software'),1,0,'L');
-$pdf->Cell(91,5,utf8_decode($actualizar_sw),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'4',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,'Formateo completo',1,0,'L');
-$pdf->Cell(91,5,utf8_decode($formateo_completo),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'5',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,'Limpieza de virus',1,0,'L');
-$pdf->Cell(91,5,utf8_decode($limpieza_virus),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'6',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,utf8_decode('Instalación de software'),1,0,'L');
-$pdf->Cell(91,5,utf8_decode($instalar_sw),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'6',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,'Otra',1,0,'L');
-$pdf->Cell(91,5,utf8_decode($otra_sw),1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','B',10);
-$pdf->Cell(191,8,'OTRAS',1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'3',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,utf8_decode('Escaneo de documentos y/o imágenes'),1,0,'L');
-$pdf->Cell(91,5,utf8_decode($escanear),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'4',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,utf8_decode('Impresión a color'),1,0,'L');
-$pdf->Cell(91,5,utf8_decode($printcolor),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'5',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,utf8_decode('Grabar información en CDs o DVDs'),1,0,'L');
-$pdf->Cell(91,5,utf8_decode($rw_cd),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'6',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,utf8_decode('Publicar información en el sitio web oficial'),1,0,'L');
-$pdf->Cell(91,5,utf8_decode($web),1,0,'C');
-$pdf->Ln();
-$pdf->Cell(10,5,'6',1,0,'C');
-$pdf->SetFont('Arial','',8);
-$pdf->Cell(90,5,'Otra',1,0,'L');
-$pdf->Cell(91,5,utf8_decode($otra_2),1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','B',8);
-$pdf->Cell(191,10,'OBSERVACIONES:',1,0,'L');
-$pdf->Ln();
-$pdf->SetFont('Arial','B',8);
-$pdf->Cell(100,5,'Realiza mantenimiento',1,0,'C');
-$pdf->Cell(91,5,'Recibe de conformidad',1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','B',8);
-$pdf->Cell(100,20,'',1,0,'C');
-$pdf->Cell(91,20,utf8_decode($rowSQL['datos_usr']),1,0,'C');
-$pdf->Ln();
-$pdf->SetFont('Arial','B',8);
-$pdf->Cell(100,5,'Nombre y firma',1,0,'C');
-$pdf->Cell(91,5,'Nombre y firma',1,0,'C');
-$modo="I";
-$nombre_archivo="reporte_servicio.pdf";
-$pdf->Output($nombre_archivo,$modo);
-//$pdf->Output($modo,$nombre_archivo);
 
-                                  //ob_end_flush();
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(14,5,utf8_decode('Nombre completo:'),0,0,'L');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(125,5,utf8_decode(''),0,0,'C');
+$pdf->Ln();
 
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(18,5,utf8_decode('Domicilio:'),0,0,'L');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(105,5,utf8_decode(''),0,0,'C');
+$pdf->Cell(2,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(13,5,utf8_decode('No. Ext:'),0,0,'R');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(18,5,utf8_decode(''),0,0,'C');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(13,5,utf8_decode('No. Int.:'),0,0,'R');
+$pdf->Cell(1,6,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(18,5,utf8_decode(''),0,0,'C');
+$pdf->Ln();
+
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(15,5,utf8_decode('Colonia:'),0,0,'L');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(75,5,utf8_decode(''),0,0,'C');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(26,5,utf8_decode('Entre vialidades:'),0,0,'R');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(72,5,utf8_decode(''),0,0,'C');
+$pdf->Ln();
+
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(13,5,utf8_decode('Estado:'),0,0,'L');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(30,5,utf8_decode(''),0,0,'C');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(17,5,utf8_decode('Municipio:'),0,0,'L');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(55,5,utf8_decode(''),0,0,'C');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(17,5,utf8_decode('Localidad:'),0,0,'R');
+$pdf->Cell(1,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(54,5,utf8_decode(''),0,0,'C');
+$pdf->Ln();
+
+$pdf->Ln();
+$pdf->SetFont('Arial','',9);
+$pdf->Multicell(190,5,utf8_decode('Recibí del Gobierno del Estado de Zacatecas, a través del Instituto para la Atención e Inclusión de las Personas con Discapacidad el(los) artículo(s) que abajo se lista(n) en calidad de Donativo a mi favor.'),0,'J',0);
+$pdf->Multicell(190,5,utf8_decode('Mismo(s) que me comprometo a darle buen uso, así como limpieza y mantenimiento en su caso, asimismo, reintegrarlo una vez que deje de ser utilizado por el o la beneficiaria.'),0,'J',0);
+$pdf->Ln();
+
+/* Insertar tabla de servicios */
+
+$pdf->Ln();
+$pdf->SetFont('Arial','B',9);
+$pdf->Cell(72,5,utf8_decode('Firma de recibido: '),0,0,'R');
+$pdf->Cell(2,5,utf8_decode(''),0,0,'C');
+$pdf->SetFont('Arial','',9);
+$pdf->Cell(93,5,utf8_decode('_________________________________________________'),0,0,'L');
+$pdf->Ln();
+
+
+
+$pdf->Output();
 ?>
