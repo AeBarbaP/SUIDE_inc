500fb1a943bcfd3e0fff0fac594816ed29302577
AnnyBarba <aebarbap@gmail.com>
2024-02-13 13:46:15 -0600

    igual que ayer

--- query/queryEnfermedadesBadges.php
+++ query/queryEnfermedadesBadges.php
@@ -12,6 +12,5 @@
     echo '
         <button class="badge btn btn-sm rounded-pill text-bg-secondary" id="E'.$id.'"><span id="'.$id.'" class="valorFull">'.$nombre.'</span> <a class="text-light" onclick="removeA(\'E'.$id.'\')"><i class="bi bi-x-circle"></i></a></button>
     ';
-    
 
-    ?>
+?>
