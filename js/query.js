2dd5030b534063601c9b9eac2aeb8781de86f772
AnnyBarba <aebarbap@gmail.com>
2024-07-10 15:18:27 -0600

    buscando error en produccion

--- js/query.js
+++ js/query.js
@@ -4,7 +4,6 @@ function tablaPCDFull(){
         type:"POST",
         url:"query/queryPadronFull.php",
         dataType: "html",
-        cache: false,
         success: function(response) { 
             $('#myTablePCD').html(response);
         }
