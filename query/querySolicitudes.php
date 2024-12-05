5d5784894e66f3ab56fc54be27691b71450c005f
AnnyBarba <aebarbap@gmail.com>
2024-12-05 15:31:11 -0600

    actaER en proceso para solicitudes

--- query/querySolicitudes.php
+++ query/querySolicitudes.php
@@ -9,7 +9,7 @@
 
     if($filas > 0){
         while($rowSQL=$resultadoSQL->fetch_assoc()){
-            
+            $idrow1 = $rowSQL['id'];
             echo'
                 <tr>
                     <td>'.$rowSQL['folio_solicitud'].'</td>
@@ -22,8 +22,8 @@
                             <td>Credencial</td>
                             <td>Entregado</td>
                             <td>'.$rowSQL['entrega'].'</td>
-                            <td></td>
-                            <td><button class="btn btn-warning" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
+                            <td><a href="prcd/actaER_PDF.php?curp='.$curp.'&id='.$idrow1.'&flag=1"><i class="bi bi-file-earmark-text-fill"></i> Acta de entrega</a></td>
+                            <td><button class="btn btn-primary" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
                         ';
                     } 
                     else if ($tipoNombre == 5){
@@ -32,8 +32,8 @@
                             <td>Tarjetón</td>
                             <td>Entregado</td>
                             <td>'.$rowSQL['entrega'].'</td>
-                            <td></td>
-                            <td><button class="btn btn-warning" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
+                            <td><a href="prcd/actaER_PDF.php?curp='.$curp.'&id='.$idrow1.'&flag=1"><i class="bi bi-file-earmark-text-fill"></i> Acta de entrega</a></td>
+                            <td><button class="btn btn-primary" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
                         ';
                     } 
                     
@@ -52,91 +52,75 @@
 
     $sqlSolicitud = "SELECT * FROM servicios WHERE curp = '$curp'";
     $resultadosqlServicios = $conn->query($sqlSolicitud);
-    $rowSqlServicios = $resultadosqlServicios->fetch_assoc();
-    $filas = $resultadosqlServicios->num_rows;
-
-    if ($filas >= 1) {
-        $detalle = $rowSqlServicios['detalle_solicitud'];
-
-        $sqlServicios = "SELECT * FROM funcionales WHERE id = '$detalle'";
-        $resultadoServicios = $conn->query($sqlServicios);
-        $rowServicios = $resultadoServicios->fetch_assoc();
-
-        $nombre = $rowServicios['nombre'];
-        $estatusN = $rowSqlServicios['estatus'];
-
     
-        if ($tipoNombre == 1){
+    $filas = $resultadosqlServicios->num_rows;
+    
+    if ($filas >= 1) {
+        
+        
+        while ($rowSqlServicios = $resultadosqlServicios->fetch_assoc()){
+            $detalle = $rowSqlServicios['detalle_solicitud'];
+            $idrow = $rowSqlServicios['id'];
+
+            $sqlServicios = "SELECT * FROM funcionales WHERE id = '$detalle'";
+            $resultadoServicios = $conn->query($sqlServicios);
+            $rowServicios = $resultadoServicios->fetch_assoc();
+            
+            $folio = $rowSqlServicios['folio_solicitud']; 
+            $fecha = $rowSqlServicios['fecha_solicitud'];
+            $tipo_sol = $rowSqlServicios['tipo_solicitud'];
+            $nombre = $rowServicios['nombre'];
+            $estatusN = $rowSqlServicios['estatus_s'];
+            
+            if ($tipo_sol == 1){
+                $tipo = "Funcional";
+            }
+            else if ($tipo_sol == 2){
+                $tipo = "Extraordinario";
+            }
+            else if ($tipo_sol == 3){
+                $tipo = "Otro";
+            }
+
+            /* if ($tipoNombre == 1){ */
             echo '
-                <td>Funcionales</td>
+            <tr>
+                <td>'.$folio.'</td>
+                <td>'.$fecha.'</td>
+                <td>'.$tipo.'</td>
                 <td>'.$nombre.'</td>
             ';
+            $fechaentrega = $rowSqlServicios['fecha_entrega'];
+            if ($fechaentrega == null){
+                $entrega = "";
+            }
+            else {
+                $entrega = $rowSqlServicios['fecha_entrega'];
+            }
+
             if ($estatusN == 1){
                 echo '
                     <td>Entregado</td>
-                    <td>'.$rowSQL['entrega'].'</td>
-                    <td><a><i class="bi bi-file-earmark-text-fill"></i> Acta de entrega</a></td>
-                    <td><button class="btn btn-warning" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
+                    <td>'.$entrega.'</td>
+                    <td><a href="prcd/actaER_PDF.php?curp='.$curp.'&id='.$idrow.'&flag=2"><i class="bi bi-file-earmark-text-fill"></i> Acta de entrega</a></td>
+                    <td><button class="btn btn-primary" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
                 ';
             } 
             else {
                 echo '
                     <td>Lista de Espera</td>
-                    <td>'.$rowSQL['entrega'].'</td>
-                    <td><button class="btn btn-success" type="button">Entregar</button></td>
-                    <td><button class="btn btn-warning" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
+                    <td>'.$entrega.'</td>
+                    <td></td>
+                    <td><button class="btn btn-primary" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
                 ';
             } 
-        } 
-        else if ($tipoNombre == 2){
-            echo '
-                <td>Extraordinarios</td>
-                <td>'.$nombre.'</td>
-            ';
-            if ($estatusN == 1){
-                echo '
-                    <td>Entregado</td>
-                    <td>'.$rowSQL['entrega'].'</td>
-                    <td><a><i class="bi bi-file-earmark-text-fill"></i> Acta de entrega</a></td>
-                    <td><button class="btn btn-warning" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
-                ';
-            } 
-            else {
-                echo '
-                    <td>Lista de Espera</td>
-                    <td>'.$rowSQL['entrega'].'</td>
-                    <td><button class="btn btn-success" type="button">Entregar</button></td>
-                    <td><button class="btn btn-warning" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
-                ';
-            } 
-        } 
-        else if ($tipoNombre == 3){
-            echo '
-                <td>Otros</td>
-                <td>'.$nombre.'</td>
-            ';
-            if ($estatusN == 1){
-                echo '
-                    <td>Entregado</td>
-                    <td>'.$rowSQL['entrega'].'</td>
-                    <td><a><i class="bi bi-file-earmark-text-fill"></i> Acta de entrega</a></td>
-                    <td><button class="btn btn-warning" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
-                ';
-            } 
-            else {
-                echo '
-                    <td>Lista de Espera</td>
-                    <td>'.$rowSQL['entrega'].'</td>
-                    <td><button class="btn btn-success" type="button">Entregar</button></td>
-                    <td><button class="btn btn-warning" type="button"><i class="bi bi-arrow-repeat"></i></button></td>
-                ';
-            } 
-        } 
-    } else{
+        }
+    } 
+    else {
         echo'
-        <tr>
-            <td colspan="8" class="bg-danger text-light">Sin datos de Servicios</td>
-        </tr>
+            <tr>
+                <td colspan="8" class="bg-danger text-light">Sin datos de Servicios</td>
+            </tr>
         ';
     }
 ?>
